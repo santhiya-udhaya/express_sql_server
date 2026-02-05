b9189fe79ca697dedc2f3c8dc24262cfeae5d1b6
Santhiya udhaya <santhiyaraja488@gmail.com>
2026-02-05 09:14:25 +0530

    last

--- Db/db.js
+++ Db/db.js
@@ -1,23 +1,27 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
+
 dotenv.config();
+
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
 export const connectDB = async () => {
-    try {
-        const connection=await db.getConnection();
-        console.log("Database connected successfully");
-        connection.release();
-    } catch (err) {
-        console.error("Database connection is not established");
-        process.exit(1);
-    }
-}
+  try {
+    const connection = await db.getConnection();
+    console.log("✅ Database connected successfully");
+    connection.release();
+  } catch (err) {
+    console.error("❌ Database connection failed");
+    console.error(err); // REAL error
+  }
+};
+
 export default db;
